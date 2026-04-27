f74a87736712a5643dd9501aab9955ad97f1af77
MLGloser <dimitarmucunski@outlook.com>
2026-04-27 23:39:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from models import Book
 
 def main():
-    # Създаваме списък с обекти от класа Book
     library = [
         Book("Под игото", "Иван Вазов", 1894, 25.00),
         Book("Тютюн", "Димитър Димов", 1951, 30.00),
@@ -12,16 +11,14 @@ def main():
     for book in library:
         print(book.get_description())
 
-    # Търсене на книги след определена година (условно изражение)
     search_year = 1950
     print(f"\n--- Книги, издадени след {search_year} г. ---")
     for book in library:
         if book.year > search_year:
             print(f"Намерена: {book.title}")
 
-    # Прилагане на отстъпка за конкретна книга (използване на метод)
     print("\n--- Промоция ---")
-    print(library[0].apply_discount(10)) # 10% отстъпка за първата книга
+    print(library[0].apply_discount(10)) 
 
 if __name__ == "__main__":
     main()
